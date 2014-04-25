fd0fa3ffb900e5e18006d36e692cfbf4e095e003
creativewild <luisgomes@creativewild.net>
2014-04-25 11:51:37 +0100

    Updated names and removed old cache
    Updated names and removed old cache

--- mod_gw2wvw.php
+++ mod_gw2wvw.php
@@ -1,9 +1,12 @@
 <?php
 /**
-* WvW Scores Modules
+*  Guild Wars 2 WvW Scores Modules
 *
-* @version              $Id: mod_wvwscores.php 172 2013-05-23 01:44:00Z omniomi $
+* @version              $Id: mod_gw2wvw.php 1 2014-04-25 09:50:00Z  $
 * @license              GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+* 
+* Using API wrapper developed by Sorrow.7384
+*
 */
 
 // No direct access
@@ -16,7 +19,7 @@ use \vesu\SDK\Gw2\Gw2SDK;
 use \vesu\SDK\Gw2\TwitchException;
 $homeworld = $params->get('homeserver');
 $gw2 = new Gw2SDK(dirname(__FILE__).'/cache');
-
+//Grabs the match using the world selectd on the control panel
 $matches = $gw2->getMatchByWorldId($homeworld);
 
 foreach($matches as $match):
@@ -28,7 +31,7 @@ foreach($matches as $match):
     $end_date = implode(".",array_reverse(explode("-",array_shift(explode("T",$match->end_time)))));
 ?>
 
-<div class="wvw_match">
+<div class="match">
     <h4>Home Server: <?php echo $gw2->parseWorldName($homeworld) ?></h4><br/>
     <div style="margin-top:-10px;">Data: <?php echo $start_date ?> - <?php echo $end_date ?></div><br />
     <div class="server red" style="color: #b80202;font-weight: bold;font-size:large;"><?php echo $gw2->parseWorldName($match->red_world_id) ?>: <span style="color: #b80202;font-weight: normal;font-size:large;"><?php echo number_format($scores[0]); ?></span></div><br />
